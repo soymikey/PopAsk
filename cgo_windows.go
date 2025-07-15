77b63eb984fee424a71e27d7f697fd626bfa935c
Michael ChengMQ <soymikey0717@gmail.com>
2025-07-15 16:39:10 +0800

    feat: 优化cgo v4

--- cgo_windows.go
+++ cgo_windows.go
@@ -24,6 +24,41 @@ void sendCtrlC() {
     SendInput(2, inputs, sizeof(INPUT));
 }
 
+void sendShiftWinS() {
+    INPUT inputs[6] = {0};
+
+    // Press Shift
+    inputs[0].type = INPUT_KEYBOARD;
+    inputs[0].ki.wVk = VK_SHIFT;
+
+    // Press Windows key
+    inputs[1].type = INPUT_KEYBOARD;
+    inputs[1].ki.wVk = VK_LWIN;
+
+    // Press S
+    inputs[2].type = INPUT_KEYBOARD;
+    inputs[2].ki.wVk = 'S';
+
+    // Release S
+    inputs[3].type = INPUT_KEYBOARD;
+    inputs[3].ki.wVk = 'S';
+    inputs[3].ki.dwFlags = KEYEVENTF_KEYUP;
+
+    // Release Windows key
+    inputs[4].type = INPUT_KEYBOARD;
+    inputs[4].ki.wVk = VK_LWIN;
+    inputs[4].ki.dwFlags = KEYEVENTF_KEYUP;
+
+    // Release Shift
+    inputs[5].type = INPUT_KEYBOARD;
+    inputs[5].ki.wVk = VK_SHIFT;
+    inputs[5].ki.dwFlags = KEYEVENTF_KEYUP;
+
+    // Send all inputs at once
+    SendInput(6, inputs, sizeof(INPUT));
+}
+
+
 // 检查剪贴板是否有图片格式
 int hasClipboardImage() {
     return IsClipboardFormatAvailable(CF_BITMAP) ||
@@ -38,8 +73,13 @@ func SendCtrlC() {
 	C.sendCtrlC()
 }
 
-// SendShiftWinSAlternative 使用 keybd_event 的替代方案
+// SendShiftWinS 发送 Shift+Win+S 快捷键
 func SendShiftWinS() {
+	C.sendShiftWinS()
+}
+
+// SendShiftWinSAlternative 使用 keybd_event 的替代方案
+func SendShiftWinSAlternative() {
 	// 使用 keybd_event 可能更可靠
 	C.keybd_event(C.VK_SHIFT, 0, 0, 0)
 	C.keybd_event(C.VK_LWIN, 0, 0, 0)
